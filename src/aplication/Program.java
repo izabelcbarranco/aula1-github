c042d74b12c420ab65a82c7ba92256dc235795eb
Izabel Barranco <izabelbarranco@usp.br>
2021-04-28 20:51:51 -0300

    teste1

---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+public class Program {
+    double velocidade;
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+    
+}
